48b0d720b142cffc9e199657887300b7bb199fb1
mxar <max.rosssner@gmail.com>
2017-08-20 07:27:38 +0800

    bugfixes and performance improvements

--- src/Eva/eva.jl
+++ src/Eva/eva.jl
@@ -31,9 +31,9 @@
 	##		mr = mutation rate
 	##===================================================================================
 	function eve(optp::t_opt_prb, tc::Tuple{Int64, Float64}, gen_size::Int64, mut::Function, mr::Float64)
-		cc = [1, Inf, 0]; s = .0														# cc = current condition of the population (epoch, current error, best element)
+		const max_delta = Array{Float64, 1}(abs.(mr./optp.sp.delta))					# calculate the maximal change that can occure through mutation
+		cc = [1, Inf, Int64(0)]; s = 0.													# cc = current condition of the population (epoch, current error, best element)
 		pop = vl_rand(optp.sp, gen_size)												# generate population from the given search space
-		max_delta = abs(mr/ncbd.delta)													# calculate the maximal change that can occure through mutation
 		while cc[1] <= tc[1] && cc[2] > tc[2]											# initiate the process
 			@inbounds for i = 1:gen_size												# get the fittest subject
 				s = optp.ff(pop[i])															# evaluate the fitness of the subject
@@ -43,12 +43,12 @@
 				end
 			end
 
-			pop[1] = pop[cc[3]]															# move champion to the first place
+			pop[1] = deepcopy(pop[Int64(cc[3])])										# move champion to the first place
 			@inbounds for i = 2:gen_size												# generate a new generation from the gens of the champion
-				pop[i] = mut(pop[1], optp.sp, max_delta)									# (mutation)
+				pop[i] .= deepcopy(pop[1])
+				pop[i] .= mut(pop[i], optp.sp, max_delta)								# (mutation)
 			end
-
-			c += 1																		# update epoche counter
+			cc[1] += 1																	# update epoche counter
 		end
 		return pop[1]																	# return overall champion
 	end
@@ -79,8 +79,8 @@
 	##===================================================================================
 	function mut_default(v::Array{Float64, 1}, ncbd::t_ncbd, max_delta::Array{Float64, 1})
 		supremum = ncbd.alpha .+ ncbd.delta												# calculate the supremum (for later checks)
-		@inbounds for i=1:ncbd.n															# mutate each value inside the vector
-			v[i] += prison(randn()*max_delta[i], ncbd.alpha[i], ncbd.supremum[i])		# random mutation inside the given n dim intervall
+		@inbounds for i=1:ncbd.n														# mutate each value inside the vector
+			v[i] += op.prison(2*(rand()-.5)*max_delta[i], ncbd.alpha[i], supremum[i])	# random mutation inside the given n dim intervall
 		end
 		return v																		# return the mutation
 	end
