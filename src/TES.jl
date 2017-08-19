b042b829a4efc55c7fd6c5737c5e857ae7278b45
mxar <max.rosssner@gmail.com>
2017-08-20 07:26:36 +0800

    bugfix

--- src/TES.jl
+++ src/TES.jl
@@ -1,11 +1,11 @@
 @everywhere module TES
 	include("APL.jl")
 
-	include("src/Math/f/t_f.jl")
+	include("Math/f/t_f.jl")
 	using t_f
 	t_f.main()
 
-	include("src/Eva/t_eva.jl")
+	include("Eva/t_eva.jl")
 	using t_eva
 	t_eva.main()
 end
