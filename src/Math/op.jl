121f1cdd940cd1abc76306c3ed3905266f6afba2
mxar <max.rosssner@gmail.com>
2017-08-23 18:10:36 +0800

    ul_imp add & subtract

--- src/Math/op.jl
+++ src/Math/op.jl
@@ -2,7 +2,7 @@
     ##===================================================================================
     ## aggregations
     ##===================================================================================
-    export PI, impure_add, impure_subtract
+    export PI, imp_add, lr_imp_add, imp_sub, lr_imp_sub
 
     ##-----------------------------------------------------------------------------------
     function PI{T<:Real}(N::Array{T, 1})
@@ -12,7 +12,7 @@
     end
 
     ##-----------------------------------------------------------------------------------
-    function impure_add(V1, V2)
+    function imp_add(V1, V2)
         L = [length(V1), length(V1)]
         V = zeros(maximum(L))
         V[1:L[1]] += V1
@@ -21,12 +21,42 @@
     end
 
     ##-----------------------------------------------------------------------------------
-    function impure_subtract{T<:Real}(V1::Array{T, 1}, V2::Array{T, 1})
+    function lr_imp_add(m1::Array{Float64, 2}, m2::Array{Float64, 2})
+        s = (size(m1), size(m2))
+        d = (s[1][1]-s[2][1], s[1][2]-s[2][2])
+        r = Array{Float64, 2}(s[1])
+        for i = 1:s[1][1], j = 1:s[1][2]
+            if i>d[1] && j>d[2]
+                r[i, j] = m1[i, j] + m2[i-d[1], j-d[2]]
+            else
+                r[i, j] = m1[i, j]
+            end
+        end
+        return r
+    end
+
+    ##-----------------------------------------------------------------------------------
+    function imp_subt{T<:Float64}(V1::Array{Float64, 1}, V2::Array{Float64, 1})
         L = [length(V1), length(V1)]
         V = zeros(maximum(L))
         V[1:L[1]] += V1
         V[1:L[2]] -= V2
         return V
+    end
+
+    ##-----------------------------------------------------------------------------------
+    function lr_imp_sub(m1::Array{Float64, 2}, m2::Array{Float64, 2})
+        s = (size(m1), size(m2))
+        d = (s[1][1]-s[2][1], s[1][2]-s[2][2])
+        r = Array{Float64, 2}(s[1])
+        for i = 1:s[1][1], j = 1:s[1][2]
+            if i>d[1] && j>d[2]
+                r[i, j] = m1[i, j] - m2[i-d[1], j-d[2]]
+            else
+                r[i, j] = m1[i, j]
+            end
+        end
+        return r
     end
 
 
