cf58cdf3e8d5a65ccc12f88af89458fda61a45a2
mxar <max.rosssner@gmail.com>
2017-08-26 19:38:04 +0800

    done with qrd :)

--- src/Math/f/f.jl
+++ src/Math/f/f.jl
@@ -55,19 +55,19 @@
     ##===================================================================================
     ##  QR (faster than vanilla)
     ##===================================================================================
-    export qrd
+    export qrd_sq, qrd
 
     ##-----------------------------------------------------------------------------------
-    function qrd(m::Array{Float64, 2})
+    function qrd_sq(m::Array{Float64, 2})
     	s = size(m, 1)
     	t = zeros(s, s)
     	v = zeros(s)
     	r = copy(m)
     	w = 0.
 
-    	@inbounds for i=1:s
+    	for i=1:s
     		w = 0.
-    		@inbounds for j=i:s
+    		for j=i:s
     			v[j] = r[j, i]
     			w += v[j]*v[j]
     		end
@@ -75,21 +75,87 @@
     		v[i] += (r[1, i] >= 0 ? 1. : -1.)*sqrt(w)
     		w = 0.
 
-    		@inbounds for j=i:s w += v[j]*v[j] end
+    		for j=i:s w += v[j]*v[j] end
     		w = 2.0/w
 
-    		@inbounds for j=1:s, k=1:s
+    		for j=1:s, k=1:s
     			t[j, k] = k == j ? 1. : 0.
     			if j>=i && k>=i
     			    t[j, k] -= w*v[j]*v[k]
     			end
     		end
 
-    		r .= t*r
+    		for j=1:s
+    			for k=1:s
+    				v[k] = r[k, j]
+    			end
+
+    			for l=1:s
+    				w = 0.
+    				for h=1:s
+    					w += v[h]*t[l, h]
+    				end
+    				r[l, j] = w
+    			end
+    		end
     	end
 
-    	@inbounds for j=1:(s-1), k=(j+1):s
-    		r[k, j] = 0.
+    	for j=1:(s-1), k=(j+1):s
+    	 	r[k, j] = 0.
+    	end
+
+    	return (m*inv(r), r)
+    end
+
+	##-----------------------------------------------------------------------------------
+    function qrd(m::Array{Float64, 2})
+    	s = size(m, 1)
+    	t = zeros(s, s)
+    	v = zeros(s)
+    	r = copy(m)
+    	w = 0.
+
+    	for i=1:s
+    		w = 0.
+    		for j=i:s
+    			v[j] = r[j, i]
+    			w += v[j]*v[j]
+    		end
+
+    		v[i] += (r[1, i] >= 0 ? 1. : -1.)*sqrt(w)
+    		w = 0.
+
+    		for j=i:s w += v[j]*v[j] end
+    		w = 2.0/w
+
+    		for j=1:s, k=1:s
+    			t[j, k] = k == j ? 1. : 0.
+    			if j>=i && k>=i
+    			    t[j, k] -= w*v[j]*v[k]
+    			end
+    		end
+
+    		if i == 1
+    			r .= t*r
+    		else
+    			for j=1:s
+    				for k=1:s
+    					v[k] = r[k, j]
+    				end
+
+    				for l=1:s
+    					w = 0.
+    					for h=1:s
+    						w += v[h]*t[l, h]
+    					end
+    					r[l, j] = w
+    				end
+    			end
+    		end
+    	end
+
+    	for j=1:(s-1), k=(j+1):s
+    	 	r[k, j] = 0.
     	end
 
     	return (m/r, r)
