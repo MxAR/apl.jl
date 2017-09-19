4124f0d678d19dfd50c965490a40e7177a2e0fc0
mxar <max.rosssner@gmail.com>
2017-09-19 10:19:58 +0800

    collection of moving average algorithms

---
+++ src/MarketIndicators/mavg.jl
@@ -0,0 +1,68 @@
+@everywhere module mavg
+	##===================================================================================
+	##	sma (simple moving average)
+	##===================================================================================
+	export sma
+
+	##-----------------------------------------------------------------------------------
+	function sma(v::Array{Float64, 1}, p::Int64, n::Int64, start::Int64, stop::Int64)
+		r = zeros(stop-start+1)
+		s = Int64(0)
+		np = n * p
+
+		for i = start:stop
+			s = s + 1
+			for j = i:(-n):(i-np+1)
+				r[s] = r[s] + v[j]
+			end
+			r[s] = r[s]/(np)
+		end
+
+		return r
+	end
+
+
+	##===================================================================================
+	##	lma (linear moving average (weighted average))
+	##===================================================================================
+	export lma
+
+	##-----------------------------------------------------------------------------------
+	function lma(v::Array{Float64, 1}, pivot_weight::Float64, slope::Float64, p::Int64, n::Int64, start::Int64, stop::Int64)
+		r = zeros(stop-start+1)
+		s = Int64(0)
+		np = n * p
+
+		for i = start:stop
+			s = s + 1
+			for j = i:(-n):(i-np+1)
+				r[s] = r[s] + (v[j] * (pivot_weight + slope*(i-j)))
+			end
+		end
+
+		return r
+	end
+
+
+	##===================================================================================
+	##	ema (exponential moving average (weighted average))
+	##===================================================================================
+	export ema
+
+	##-----------------------------------------------------------------------------------
+	function ema(v::Array{Float64, 1}, pivot_weight::Float64, slope::Float64, p::Int64, n::Int64, start::Int64, stop::Int64)
+		r = zeros(stop-start+1)
+		s = Int64(0)
+		np = n * p
+
+		for i = start:stop
+			s = s + 1
+			for j = i:(-n):(i-np+1)
+				r[s] = r[s] + (v[j] * pivot_weight * exp(-(slope*(i-j))^2))
+			end
+		end
+
+		return r
+	end
+
+end
