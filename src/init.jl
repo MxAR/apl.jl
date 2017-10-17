0bbb8ce5e37931a4d545d6393cb6cd322711df10
mxar <max.rosssner@gmail.com>
2017-10-17 15:57:29 +0800

    added function for nullpsace calculation

--- src/init.jl
+++ src/init.jl
@@ -1,38 +1,30 @@
-using mavg
-using macd
-using rsi
-using f
+m = rand(3,5)
 
-v = 1 + (0.1*(0.5-rand(10)))
-#v = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10.]
-println("test vector: ", v)
+function g(m::Array{Float64, 2})
+	s = size(m)
 
+	if s[1] >= s[2]
+		return zeros(s[2])
+	end
 
-function g(v::Array{Float64, 1}, current_price::Float64, pivot_weight::Float64, slope::Float64, p::Int64, n::Int64, start::Int64, stop::Int64)
-	r = zeros(stop-start+1)
-	b = [-Inf, Inf]
-	s = Int64(0)
-	np = n * p
+	r = zeros(s[2], s[2]-s[1])
 
-	for i = start:stop
-		s = s + 1
-		for j = i:(-n):(i-np+1)
-			if v[j] > b[1]
-				b[1] = v[j]
-			end
-
-			if v[j] < b[2]
-				b[2] = v[j]
+	for i = 1:s[1]
+		for j = 1:s[1]
+			if i != j
+				m[j,:] -= (m[j,i]/m[i,i]) * m[i,:]
 			end
 		end
-
-		r[s] = r[s] + (((current_price - b[2])/(b[1] - b[2])) * pivot_weight * exp(-(slope*(s-1))^2))
-		b = [-Inf, Inf]
+		m[i,:] /= m[i,i]
 	end
+
+	r[1:s[1], :] = -m[:,(s[1]+1):s[2]]
+	r[(s[1]+1):end, :] = eye(s[2]-s[1])
 
 	return r
 end
 
-
-println(g(v, 1., .5, -.1, 3, 1, 3, 10))
-println(@code_warntype g(v, 1., .5, -.1, 3, 1, 3, 10))
+println(size(m))
+println(m)
+println(g(m))
+#println(@code_warntype g(m))
