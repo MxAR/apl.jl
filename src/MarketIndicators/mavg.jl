66f96126284d0b97ddc985c1be90bc6a61ca58a1
mxar <max.rosssner@gmail.com>
2017-10-30 08:19:54 +0800

    improved versions of the ema and added emai

--- src/MarketIndicators/mavg.jl
+++ src/MarketIndicators/mavg.jl
@@ -50,19 +50,21 @@
 	export ema
 
 	##-----------------------------------------------------------------------------------
-	function ema(v::Array{Float64, 1}, pivot_weight::Float64, slope::Float64, p::Int64, n::Int64, start::Int64, stop::Int64)
-		r = zeros(stop-start+1)
-		s = Int64(0)
-		np = n * p
+	function ema(v::Array{Float64, 1}, l::Int64, n::Int64 = 1)
+		u = zeros(l)
 
-		for i = start:stop
-			s = s + 1
-			for j = i:(-n):(i-np+1)
-				r[s] = r[s] + (v[j] * pivot_weight * exp(-(slope*(i-j))^2))
-			end
+		u[1] = 1.
+		u[2] = (l-1)/(l+1)
+
+		for i = 3:(l)
+			u[i] = u[i-1]*u[2]
 		end
 
-		return r
+		return (2.0*BLAS.dot(l, u, 1, v, n))/(l+1)
 	end
 
+	##-----------------------------------------------------------------------------------
+	function emai(v::Array{Float64, 1}, emav::Float64, l::Int64)						# calculates current ema based on last ema
+		return ((2*v[1])+((l-1)*emav))/(l+1)
+	end
 end
