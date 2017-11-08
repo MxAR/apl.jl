13eaadd42a0f0efb54ddf2b90f9cbb4e04f79dcd
mxar <max.rosssner@gmail.com>
2017-11-08 17:29:08 +0800

    implementation for dice sampling

--- src/init.jl
+++ src/init.jl
@@ -1,29 +1,12 @@
 using Plots
 using APL
 
-l = 25000
+v = zeros(40)
+n = 1000000
+j = 1
+d = 0
 
-V = zeros(l); V[1] = 10
-for i = 2:l; V[i] = V[i-1]+(randn()*0.01); end
+v = v/(n+d)
 
-v = log.(V)
-plot(1:l, v)
+plot(v)
 savefig("p_p.png")
-
-
-k = 1
-d = zeros(v)
-
-for i = 1:l
-	if i <= k
-		d[i] = 0
-	else
-		d[i] = v[i]-v[i-k]
-	end
-end
-
-m = median(d)
-s = 2.5*ones(d)*std(d)
-
-plot(1:l, hcat(d,s+m,m-s))
-savefig("p_d.png")
