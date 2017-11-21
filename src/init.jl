f6eb57c3e6f22259e14d5b3e760f599e751b170f
mxar <max.rosssner@gmail.com>
2017-11-21 10:51:02 +0800

    implementation of the gram schmidt process

--- src/init.jl
+++ src/init.jl
@@ -1,12 +1,29 @@
 using Plots
 using APL
 
-v = zeros(40)
-n = 1000000
-j = 1
-d = 0
+l = 25000
 
-v = v/(n+d)
+V = zeros(l); V[1] = 10
+for i = 2:l; V[i] = V[i-1]+(randn()*0.01); end
 
-plot(v)
+v = log.(V)
+plot(1:l, v)
 savefig("p_p.png")
+
+
+k = 1
+d = zeros(v)
+
+for i = 1:l
+	if i <= k
+		d[i] = 0
+	else
+		d[i] = v[i]-v[i-k]
+	end
+end
+
+m = median(d)
+s = 2.5*ones(d)*std(d)
+
+plot(1:l, hcat(d,s+m,m-s))
+savefig("p_d.png")
