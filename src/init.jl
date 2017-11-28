d19c694801c9db11d58f2c67e57c7602c2321162
mxar <max.rosssner@gmail.com>
2017-11-28 10:34:06 +0800

    BEST camp work

--- src/init.jl
+++ src/init.jl
@@ -1,29 +1,59 @@
-using Plots
+using Wavelets
+using TimeWarp
+using TimeWarp.WarpPlots
+using MDCT
 using APL
 
+### SETUP BEGIN ###
 l = 25000
+u = zeros(l)
+v = zeros(l)
 
-V = zeros(l); V[1] = 10
-for i = 2:l; V[i] = V[i-1]+(randn()*0.01); end
-
-v = log.(V)
-plot(1:l, v)
-savefig("p_p.png")
-
-
-k = 1
-d = zeros(v)
-
-for i = 1:l
-	if i <= k
-		d[i] = 0
-	else
-		d[i] = v[i]-v[i-k]
-	end
+for i = 2:l
+	u[i] = u[i-1]+(randn()*0.01)
+	v[i] = v[i-1]+(randn()*0.01)
 end
 
-m = median(d)
-s = 2.5*ones(d)*std(d)
+v += 1+(circshift(-u, 100)+(randn(l)*0.01))
+u .+= 1
 
-plot(1:l, hcat(d,s+m,m-s))
-savefig("p_d.png")
+sv = AbstractArray{Float64, 1}(v[1:12500])
+su = u[1:12500]
+
+tv = v[12501:end]
+tu = u[12501:end]
+### SETUP END ###
+
+### MDCT BEGIN ###
+dv = mdct(sv)
+du = mdct(su)
+### MDCT END ###
+
+### OMPA BEGIN ###
+function mm(c::Array{Float64, 1}, il::Int64, si::Int64)
+	cl = length(c)
+	r = zeros(cl, il)
+
+	for i = 1:cl
+		for j = 1:il
+
+			r[i, j] = (1/cl)*c[i]*cos((pi/cl)*(j-0.5+(cl/2))*(si+i+0.5))
+
+		end
+	end
+
+	return r
+end
+
+p = 0
+
+mv = mm(dv, 6250, 0+p)
+mu = mm(du, 6250, 0+p)
+
+rv = matchingpursuit(sv[(1+p):(6250+p)], (x)->mv*x, (x)->mv'*x, 1)
+ru = matchingpursuit(su[(1+p):(6250+p)], (x)->mu*x, (x)->mu'*x, 1)
+### OMPA END ###
+
+### DTW BEGIN ###
+plot([fastdtw(rv, ru, radius)[1] for radius=1:100, i=1:10 ], show=true)
+### DTW END ###
