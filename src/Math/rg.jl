b589704194d9b6079bc08a68516a42956eed4e61
mxar <max.rosssner@gmail.com>
2017-12-31 16:48:27 +0100

    refactoring of the rg module

--- src/Math/rg.jl
+++ src/Math/rg.jl
@@ -8,24 +8,24 @@
     ##===================================================================================
     ## logistic regression
     ##===================================================================================
-    export llog_rg
+    export rg_log
 
     ##-----------------------------------------------------------------------------------
-    function llog_rg{T<:Real}(Points::Array{T, 2})                                      # last element in a row is y the rest are x's
-        reve = zeros(2, 1)
-        coeff = zeros(2, 2)
-        rows = size(Points, 1)
-        L = ceil(maximum(Points[1:rows,2]))
+    function rg_log{T<:AbstractFloat}(X::Array{T, 2})                                      # last column is y the rest are x's
+        reve = zeros(T, 2, 1)
+        coeff = zeros(T, 2, 2)
+        rows = size(X, 1)
+        L = ceil(maximum(X[1:rows,2]))
 
-        coeff[1, 1] = size(Points, 1)
-        coeff[1, 2] = sum(Points[1:rows,1])
-        coeff[2, 2] = sumabs2(Points[1:rows,1])
+        coeff[1, 1] = size(X, 1)
+        coeff[1, 2] = sum(X[1:rows,1])
+        coeff[2, 2] = sumabs2(X[1:rows,1])
         coeff[2, 1] = coeff[1, 2]
 
-        Points[1:rows,2] = map((x) -> log((L - x) / x), Points[1:rows,2])
+        X[1:rows,2] = map((x) -> log((L - x) / x), X[1:rows,2])
 
-        reve[2, 1] = bdot(Points[1:rows,1], Points[1:rows,2])
-        reve[1, 1] = sum(Points[1:rows,2])
+        reve[2, 1] = bdot(X[1:rows,1], X[1:rows,2])
+        reve[1, 1] = sum(X[1:rows,2])
 
         S = coeff \ reve
         return (x) -> L / (1 + exp(S[1] + (x*S[2])))
@@ -35,35 +35,22 @@
     ##===================================================================================
     ## linear statistic regression
     ##===================================================================================
-    export lsta_rg
+    export rg_sta
 
     ##-----------------------------------------------------------------------------------
-    function lsta_rg(X::Array{Float64, 2})
-        m = f.mmed(X)
-        a = Base.cov(X[:, 1], X[:, 2]) / Base.var(X[:, 1])
+    function rg_sta{T<:AbstractFloat}(X::Array{T, 2})
+        m = f.mamean(X)
+        a = cov(X[:, 1], X[:, 2])/var(X[:, 1])
         return [(m[2] - (a * m[1])), a]
-    end
-
-
-    ##===================================================================================
-    ## multi linear regression
-    ##===================================================================================
-    export mla_rg
-
-    ##-----------------------------------------------------------------------------------
-    function mla_rg(X::Array{Float64, 2})
-        X = hcat(ones(size(X, 1)), X[:, 1:end-1])
-        Xt = X'
-        return (Xt * X) \ (Xt * X[:, end])
     end
 
     ##===================================================================================
     ## multi linear qr decomposition regression
     ##===================================================================================
-    export mlqr_rg
+    export rg_qr
 
     ##-----------------------------------------------------------------------------------
-    function mlqr_rg(X::Array{Float64, 2})
+    function rg_qr{T<:AbstractFloat}(X::Array{T, 2})
         QR = qr(hcat(ones(size(X, 1)), X[:, 1:end-1]))
         return QR[2] \ QR[1]' * X[:, end]
     end
