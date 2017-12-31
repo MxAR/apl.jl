3898c1ebfb0402c5a9141aa504545e02190a9d70
mxar <max.rosssner@gmail.com>
2017-12-31 15:44:06 +0100

    refactoring of the op module

--- src/Math/op.jl
+++ src/Math/op.jl
@@ -5,57 +5,69 @@
     export PI, imp_add, lr_imp_add, imp_sub, lr_imp_sub
 
     ##-----------------------------------------------------------------------------------
-    function PI{T<:Real}(N::Array{T, 1})
+    function PI{T<:Number}(N::Array{T, 1})
         p = N[1]
-        for i = 2:length(N) p *= N[i] end
+
+        @inbounds for i = 2:size(N, 1)
+            p *= N[i]
+        end
+
         return p
     end
 
     ##-----------------------------------------------------------------------------------
-    function imp_add(V1, V2)
-        L = [length(V1), length(V1)]
-        V = zeros(maximum(L))
-        V[1:L[1]] += V1
-        V[1:L[2]] += V2
-        return V
+    function imp_add{T<:Number}(v1::Array{T, 1}, v2::Array{T, 1})
+        l = (size(v1, 1), size(v1, 1))
+        v = zeros(max(l))
+        v[1:l[1]] = v1
+        v[1:l[2]] += v2
+        return v
     end
 
     ##-----------------------------------------------------------------------------------
-    function lr_imp_add(m1::Array{Float64, 2}, m2::Array{Float64, 2})
+    function imp_add{T<:Number}(m1::Array{T, 2}, m2::Array{T, 2})
         s = (size(m1), size(m2))
-        d = (s[1][1]-s[2][1], s[1][2]-s[2][2])
-        r = Array{Float64, 2}(s[1])
-        for i = 1:s[1][1], j = 1:s[1][2]
-            if i>d[1] && j>d[2]
-                r[i, j] = m1[i, j] + m2[i-d[1], j-d[2]]
-            else
+        b = (max(s[1][1], s[2][1]), max(s[1][2], s[2][2]))
+        r = Array{T, 2}(i)
+
+        for i = 1:b[1], j = 1:b[2]
+            if i>s[1][1] && j>s[1][2]
                 r[i, j] = m1[i, j]
             end
+
+            if i>s[2][1] && j>s[2][2]
+                r[i, j] += m2[i, j]
+            end
         end
+
         return r
     end
 
     ##-----------------------------------------------------------------------------------
-    function imp_subt(V1::Array{Float64, 1}, V2::Array{Float64, 1})
-        L = [length(V1), length(V1)]
-        V = zeros(maximum(L))
-        V[1:L[1]] += V1
-        V[1:L[2]] -= V2
-        return V
+    function imp_sub{T<:Number}(V1::Array{T, 1}, V2::Array{T, 1})
+        l = (length(v1), length(v1))
+        v = zeros(max(l))
+        v[1:l[1]] = v1
+        v[1:l[2]] -= v2
+        return v
     end
 
     ##-----------------------------------------------------------------------------------
-    function lr_imp_sub(m1::Array{Float64, 2}, m2::Array{Float64, 2})
+    function imp_sub{T<:Number}(m1::Array{T, 2}, m2::Array{T, 2})
         s = (size(m1), size(m2))
-        d = (s[1][1]-s[2][1], s[1][2]-s[2][2])
-        r = Array{Float64, 2}(s[1])
-        for i = 1:s[1][1], j = 1:s[1][2]
-            if i>d[1] && j>d[2]
-                r[i, j] = m1[i, j] - m2[i-d[1], j-d[2]]
-            else
+        b = (max(s[1][1], s[2][1]), max(s[1][2], s[2][2]))
+        r = Array{T, 2}(i)
+
+        for i = 1:b[1], j = 1:b[2]
+            if i>s[1][1] && j>s[1][2]
                 r[i, j] = m1[i, j]
             end
+
+            if i>s[2][1] && j>s[2][2]
+                r[i, j] -= m2[i, j]
+            end
         end
+
         return r
     end
 
@@ -66,8 +78,8 @@
     export prison
 
     ##-----------------------------------------------------------------------------------
-    prison(value, infimum, supremum) = min(max(value, infimum), supremum)
+    prison{T<:Number}(value::T, infimum::T, supremum::T) = min(max(value, infimum), supremum)
 
     ##-----------------------------------------------------------------------------------
-    prison(x, f::Function, infimum, supremum) = ifelse(x < infimum, 0, ifelse(x > supremum, 1, f(x)))
+    prison{T<:Number}(x::T, f::Function, infimum::T, supremum::T) = x < infimum ? 0 : (x > supremum ? 1 : f(x))
 end
