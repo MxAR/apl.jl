a936352a1264b1a5fb80ffb5e769512f181bc661
mxar <max.rosssner@gmail.com>
2017-12-31 16:49:49 +0100

    refactoring of the pct module

--- src/Percepton/Percepton.jl
+++ src/Percepton/Percepton.jl
@@ -9,9 +9,9 @@
     ##===================================================================================
     ##  types
     ##===================================================================================
-    type tpct                                                                           # Percepton
-        weights::Array{Real, 1}
-        threshold::Float64
+    type tpct{T<:AbstractFloat}                                                         # Percepton
+        weights::Array{T, 1}
+        threshold::T
         threshold_func::Function
     end
 
@@ -22,18 +22,18 @@
     export tpctv0, tpctv1, tpctv2
 
     ##-----------------------------------------------------------------------------------
-    function tpctv0(weights, threshold = 0.5, threshold_func::Function = t(x, y) = x >= y ? 1 : 0)
+    function tpctv0{T<:AbstractFloat}(weights::Array{T, 1}, threshold::T = .5, threshold_func::Function = t(x, y) = x >= y ? 1 : 0)
         return tpct(weights, threshold, threshold_func)
     end
 
     ##-----------------------------------------------------------------------------------
-    function tpctv1(Inputs::Int, Random = true, threshold = 0.5, threshold_func::Function = t(x, y) = x >= y ? 1 : 0)
-        @assert Inputs > 0 ["the inputs musn't be negative"]
+    function tpctv1{T<:AbstractFloat, N<:Integer}(Inputs::N, Random::Bool = true, threshold::T = .5, threshold_func::Function = t(x, y) = x >= y ? 1 : 0)
+        @assert(Inputs > 0, "the numerb of inputs musn't be negative")
         return tpct(Random ? randn(Inputs) : zeros(Inputs), threshold, threshold_func)
     end
 
     ##-----------------------------------------------------------------------------------
-    function tpctv2{T}(Inputs::Array{T, 2}, ExpectedResults::Array{T, 2}, threshold = 0.5, threshold_func::Function = t(x, y) = x >= y ? 1 : 0) # constructor which will approximate all weights
+    function tpctv2{T<:AbstractFloat}(Inputs::Array{T, 2}, ExpectedResults::Array{T, 2}, threshold::T = .5, threshold_func::Function = t(x, y) = x >= y ? 1 : 0) # constructor which will approximate all weights
         return tpct((Inputs \ ExpectedResults), threshold, threshold_func)
     end
 
@@ -44,8 +44,8 @@
     export iaf
 
     ##-----------------------------------------------------------------------------------
-    function iaf{T}(PCT::tpct, Input::Array{T, 1})
-        @assert length(PCT.weights) == length(Input) ["input array length must match weight length"]
+    function iaf{T<:AbstractFloat}(PCT::tpct, Input::Array{T, 1})
+        @assert(size(PCT.weights, 1) == size(Input, 1), "input array length must match weight length")
         return PCT.threshold_func(bdot(PCT.weights, Input), PCT.threshold);
     end
 
@@ -56,10 +56,11 @@
     export gdb!, gdo!
 
     ##-----------------------------------------------------------------------------------
-    function gdo!{T}(PCT::tpct, Inputs::Array{T, 2}, ExpectedResults::Array{T, 1}, LearningRate::T = 0.2, MaxCycles::Int = -1)
+    function gdo!{T<:AbstractFloat}(PCT::tpct, Inputs::Array{T, 2}, ExpectedResults::Array{T, 1}, LearningRate::T = 0.2, MaxCycles::Int = -1)
         InputD2Size = size(Inputs, 2)                                                   # faster than Batch with tpctv2
         CurrentInput = []
         epsilon = delta = 10
+
         while epsilon > 0 && MaxCycles != 0
             epsilon = 0
             for i = 1:size(Inputs, 1)
@@ -73,13 +74,15 @@
             end
             MaxCycles -= 1;
         end
+
         return PCT
     end
 
-    function gdb!{T}(PCT::tpct, Inputs::Array{T, 2}, ExpectedResults::Array{T, 1}, LearningRate::T = 0.2, MaxCycles::Int = -1)
+    function gdb!{T<:AbstractFloat}(PCT::tpct, Inputs::Array{T, 2}, ExpectedResults::Array{T, 1}, LearningRate::T = 0.2, MaxCycles::Int = -1)
         InputD2Size = size(Inputs, 2)
         CurrentInput = Wc = []
         epsilon = delta = Tc = 10
+
         while epsilon > 0  && MaxCycles != 0
             epsilon = Tc = 0
             Wc = zeros(InputD2Size)
@@ -96,6 +99,7 @@
             PCT.weights += Wc
             MaxCycles -= 1;
         end
+
         return PCT
     end
 
