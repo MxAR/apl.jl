2fe3be562a7be22e5f6e84267fb0e388e93f138e
mxar <max.rosssner@gmail.com>
2018-04-17 16:14:26 +0800

    added implementation for the hypoexponential distribution

--- src/Math/rnd.jl
+++ src/Math/rnd.jl
@@ -10,10 +10,24 @@
 		return -a*log.(r)
 	end
 
+
 	##===================================================================================
 	##	hypoexponential distribution
+	##		a: exponential scalar
+	##		n: number sums taken
 	##===================================================================================
-	function rhexp{R<:AbstractFloat, N<:Integer}(a::R, d::N...)
-
+	export rhexp
+	
+	##-----------------------------------------------------------------------------------
+	function rhexp{R<:AbstractFloat, N<:Integer}(a::R, n::N, d::N...)
+		if isempty(d)
+			@inbounds return -a*sum([log(rand()) for i = 1:n])	
+		else
+			r = zeros(R, d)
+			@inbounds for i = 1:n
+				r += log.(rand(d))
+			end
+			return -a*r
+		end 
 	end 
 end
