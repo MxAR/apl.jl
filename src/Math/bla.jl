96368a9eacbd8d56204239f41518b0056eb4a091
mxar <max.rosssner@gmail.com>
2018-06-01 11:46:52 +0300

    removed logisitc regression since it was kinda useless

--- src/Math/bla.jl
+++ src/Math/bla.jl
@@ -1064,34 +1064,6 @@
 	end
 
 
-    ##===================================================================================   	
-    ##	logistic regression
-    ##		last column is y the rest are x's
-	##===================================================================================
-    export rg_log
-
-    ##-----------------------------------------------------------------------------------
-    function rg_log{T<:AbstractFloat}(X::Array{T, 2})
-        reve = zeros(T, 2, 1)
-        coeff = zeros(T, 2, 2)
-        rows = size(X, 1)
-        L = ceil(maximum(X[1:rows,2]))
-
-        coeff[1, 1] = size(X, 1)
-        coeff[1, 2] = sum(X[1:rows,1])
-        coeff[2, 2] = sumabs2(X[1:rows,1])
-        coeff[2, 1] = coeff[1, 2]
-
-        X[1:rows,2] = map((x) -> log((L - x) / x), X[1:rows,2])
-
-        reve[2, 1] = bdot(X[1:rows,1], X[1:rows,2])
-        reve[1, 1] = sum(X[1:rows,2])
-
-        S = coeff \ reve
-        return (x) -> L / (1 + exp(S[1] + (x*S[2])))
-    end
-
-
     ##===================================================================================
     ## linear statistic regression
     ##===================================================================================
